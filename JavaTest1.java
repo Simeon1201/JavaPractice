b29dfbaa48821dc238fa80c8b2c497af05536a3b
Simeon1201 <simeon.dyankov@gmail.com>
2026-08-16 23:02:10 -0500

    Refactor JavaTest1 for commit practice using a stream

--- JavaTest1.java
+++ JavaTest1.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class JavaTest1 {
     public static void main() {
-        System.out.println("Hello, World!");
+        System.out.println("testing stream");
         
-        // Simple calculation
-        int a = 10;
-        int b = 25;
-        int sum = a + b;
+        int[] numbers = {2, 3, 12, 1, 5, 6, 17, 8, 13, 21};
         
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        int sumOfEvens = Arrays.stream(numbers)
+                .filter(n -> n % 2 == 0)
+                .sum();
+    
+        System.out.println("Sum of even numbers: " + sumOfEvens);
     }
+
 }
