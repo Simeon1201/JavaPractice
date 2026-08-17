72d4194e54c85cd8d2ad987f135df4ffbda3fc39
Simeon1201 <simeon.dyankov@gmail.com>
2026-08-16 22:49:45 -0500

    JavaTesting1

---
+++ JavaTest1.java
@@ -0,0 +1,12 @@
+public class JavaTest1 {
+    public static void main() {
+        System.out.println("Hello, World!");
+        
+        // Simple calculation
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
